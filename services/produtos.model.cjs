dbcd2b751a656dd0f821b68be6c1572577722996
Luis <1luisfelipe2@gmail.com>
2022-09-25 22:50:28 -0400

    adicionado camada de serviços

---
+++ services/produtos.model.cjs
@@ -0,0 +1,39 @@
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize(
+  "mysql://sa7f483b4shxxrq7:j9uazobqudc0zw61@qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zpzigit5o0jticwb"
+);
+
+const { DataTypes } = require("sequelize");
+const Produtos = sequelize.define(
+  "Produtos",
+  {
+    produto_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    valor: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    categoria: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ativo: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = {
+  Produtos,
+};
