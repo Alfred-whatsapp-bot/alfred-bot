5100eb379043ea43ff97c68424ceb04afbb47935
Luis Felipe Amorim Sant'Ana <1luisfelipe2@gmail.com>
2022-09-30 11:14:44 -0400

    teste de implementacao do MENU

--- src/stages/0.js
+++ src/stages/0.js
@@ -1,7 +1,8 @@
+import { getAllProdutos } from "../../repository/repository.mjs";
 import { storage } from "../storage.js";
 
 export const initialStage = {
-  exec({ from, client }) {
+  async exec({ from, client }) {
     storage[from].stage = 1;
 
     const buttons = [
@@ -17,9 +18,7 @@ export const initialStage = {
       },
     ];
 
-    console.log(buttons);
-
-    client
+    await client
       .sendButtons(from, "Olá, seja bem-vindo(a)!", buttons, "O que deseja?")
       .then((result) => {
         console.log("Result: ", result); //return object success
@@ -27,5 +26,50 @@ export const initialStage = {
       .catch((erro) => {
         console.error("Error when sending: ", erro); //return object error
       });
+
+    // Send List menu
+    //This function does not work for Bussines contacts
+    await getAllProdutos().then(async (data) => {
+      const list = data.map((item) => {
+        return {
+          id: item.produto_id,
+          nome: item.nome,
+          categoria: item.categoria,
+          valor: item.valor,
+        };
+      });
+
+      let listMenu = [];
+      for (const element of list) {
+        listMenu = [
+          {
+            title: element.categoria,
+            rows: [
+              {
+                title: element.nome,
+                description: element.valor,
+              },
+            ],
+          },
+        ];
+      }
+      console.log(listMenu);
+
+      await client
+        .sendListMenu(
+          from,
+          "Title",
+          "subTitle",
+          "Description",
+          "menu",
+          listMenu
+        )
+        .then((result) => {
+          console.log("Result: ", result); //return object success
+        })
+        .catch((erro) => {
+          console.error("Error when sending: ", erro); //return object error
+        });
+    });
   },
 };
