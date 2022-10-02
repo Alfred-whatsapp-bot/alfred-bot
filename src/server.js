e5a32b0a19c17b33948e7476a5a2222ade4fa237
Luis <1luisfelipe2@gmail.com>
2022-10-02 13:15:14 -0400

    removido browser arguments

--- src/server.js
+++ src/server.js
@@ -10,9 +10,9 @@ create({
   multidevice: false,
   headless: true,
   executablePath: "/usr/bin/chromium-browser",
-  browserArgs: [
-    "--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
-  ],
+  // browserArgs: [
+  //   "--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+  // ],
 })
   .then((client) => start(client))
   .catch((error) => {
