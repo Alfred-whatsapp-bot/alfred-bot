7b8c291876a46b5734ea1e49449ebbdde821c0a8
Luis Felipe Amorim Sant'Ana <1luisfelipe2@gmail.com>
2022-10-20 17:11:09 -0400

    add cliente repository com metodos basicos

---
+++ repository/clienteRepository.mjs
@@ -0,0 +1,56 @@
+import { Cliente } from "../services/cliente.model.cjs";
+
+export const getAllClientes = async () => {
+  const clientes = await Cliente.findAll();
+  return clientes.map((cliente) => cliente.toJSON());
+};
+
+export const getClienteByPhoneNumber = async (phoneNumber) => {
+  const cliente = await Cliente.findAll({
+    where: {
+      telefone: phoneNumber,
+    },
+  });
+  return cliente.map((produto) => produto.toJSON());
+};
+
+export const getClienteByNome = async (nome) => {
+  const cliente = await Cliente.findAll({
+    where: {
+      nome: nome,
+    },
+  });
+  return cliente.map((produto) => produto.toJSON());
+};
+
+export const getClienteById = async (id) => {
+  const cliente = await Cliente.findAll({
+    where: {
+      cliente_id: id,
+    },
+  });
+  return cliente.map((produto) => produto.toJSON());
+};
+
+export const getClienteByCpf = async (cpf) => {
+  const cliente = await Cliente.findAll({
+    where: {
+      cpf: cpf,
+    },
+  });
+  return cliente.map((produto) => produto.toJSON());
+};
+
+export const createCliente = async (cliente) => {
+  const clienteCreated = await Cliente.create(cliente);
+  return clienteCreated._modelOptions.instance.toJSON();
+};
+
+export const updateCliente = async (cliente, cpf) => {
+  const clienteUpdated = await Cliente.update(cliente, {
+    where: {
+      cpf: cpf,
+    },
+  });
+  return clienteUpdated;
+};
