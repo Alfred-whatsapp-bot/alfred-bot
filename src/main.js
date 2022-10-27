11045823c4a28b9fc9a6b751cc649640c662bdc8
Luis <1luisfelipe2@gmail.com>
2022-10-27 18:38:10 -0400

    ajuste

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { session } from "./server";
+import { session } from "./server.js";
 import { stages } from "./stages";
 // import conversation1 from './conversations/conversation1.js';
 // import conversation2 from './conversations/conversation2.js';
